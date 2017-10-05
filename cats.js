50c75173763b3139dcd2b5816d688ef535ea1aab
Sergey Fedof <7ff77ff@gmail.com>
2017-10-05 14:12:08 +0300

    example mongodb

---
+++ cats.js
@@ -0,0 +1,55 @@
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/cat_app");
+
+var catSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    mood: String
+});
+
+var Cat = mongoose.model("Cat", catSchema);
+
+
+/*
+// Create instance of Cat
+var george = new Cat({
+    name: "Mrs. Norris",
+    age: 0000,
+    mood: "Evil"
+});
+
+// Save the Cat instance to the database, check for error
+george.save(function(err, cat){
+    if(err){
+        console.log("Something went wrong");
+    } else {
+        console.log(cat + "saved to the database");
+    }
+});
+
+*/
+// Better way to do new and save at once
+Cat.create({
+    name: "Sparkles",
+    age: 5,
+    mood: "Sassy"
+}, function(err, cat){
+    if(err){
+        console.log(err);
+    } else {
+        console.log("ALL the cats.. create")
+        console.log(cat);
+    }
+});
+
+
+// Retrieve all of the cats from the DB
+Cat.find({}, function(err, cats){
+    if(err){
+        console.log(err);
+    } else {
+        console.log("All the cats.. find")
+        console.log(cats);
+    }
+});
+
